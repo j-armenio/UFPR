2e4cd431f078f098fc3c7d139269fe7ad058eb74
JOAO ARMENIO SILVEIRA <jas21@inf.ufpr.br>
2023-01-19 19:03:18 -0300

    refazendo ex do maziero

---
+++ PROG2/wiki_maz_exercicios/remake/strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ex1(){
+    int i;
+    char *str1, *str2;
+
+    scanf("%c", )
+
+    for (i = 0; i < strlen(str1); i++){
+
+    }
+}
+
+int main(){
+
+}
