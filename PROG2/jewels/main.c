11765cfacdf9de29d34f0c509ffcc7feb806bc7b
j-armenio <joaoarmeniosilveira@gmail.com>
2023-02-08 18:53:27 -0300

    sla

---
+++ PROG2/jewels/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image-5>
+#include <allegro5/allegro_audio-5>
+#include <allegro5/allegro_acodec-5>
+#include <allegro5/allegro_primitives-5>
+#include <allegro5/allegro_ttf-5>
+
+#include "draw.h"
+#include "structs.h"
+
